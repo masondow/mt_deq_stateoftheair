4508c7865470d7011ba633d3a99fc2ba0e4a8683
cnealy-mt <railfan220@gmail.com>
2025-12-30 11:24:37 -0700

    250902 Update_CN
    Primarily added data files from EPA of AQI by county since 1980. Started this as part of a records request, but believe it could be useful for SOTA stuff.

--- scripts/data_vis/ann_avg_trends.R
+++ scripts/data_vis/ann_avg_trends.R
@@ -2,11 +2,12 @@
 library(dplyr)
 library(lubridate)
 
-pollutant <- "SO2"
+pollutant <- "Pb"
 aqs_data <- readRDS(paste0("data/aqs_data/daily_processed/", pollutant, "_daily_processed.rds"))
 
 aqs_data_filtered <- aqs_data %>%
   filter(year <= 2024) %>%
+  filter(year >= 2002) %>% #Pb only
   group_by(year) %>%
   summarize(avg_concentration = mean(arithmetic_mean, na.rm = TRUE), .groups = "drop")
 
@@ -17,9 +18,9 @@ ggplot(aqs_data_filtered, aes(x = year, y = avg_concentration)) +
   geom_line(color = "steelblue") +
   geom_point(color = "darkred") +
   labs(
-    title = "Annual Average PM2.5 Concentration",
+    title = "Annual Average SO2 Concentration",
     x = "Year",
-    y = "Avg Concentration (µg/m³)",
+    y = "Avg Concentration",
     caption = "Source: AQS data"
   ) +
   theme_minimal()
